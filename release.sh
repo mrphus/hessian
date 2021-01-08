39d396f13a88f193f48912bb84a3107829e22adc
Stefan Langeland <stefan.langeland.ext@msg-gillardon.de>
2021-01-08 18:32:05 +0100

    small release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export VERSION=$1
+
+if [[ -z "$VERSION" ]]; then
+  echo "ERROR: Version is undefined"
+  exit 1
+fi
+
+mvn versions:set -DnewVersion=${VERSION} -DgenerateBackupPoms=false
+mvn clean deploy --settings ~/.m2/ossrh.xml
+git checkout .
+git tag $VERSION
+git push tags
